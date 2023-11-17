7775175581798d7641143dbd1632817256c614cf
ayoub ezzaaraoui <146847154+ezzaaraoui@users.noreply.github.com>
2023-11-17 02:02:37 +0100

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -9,6 +9,3 @@ class Employee :
     def calculate_net_salary(self):
         net_salary = self.base_salary + self.bonus_hrs 
         print("Hi ",self.name," your net salary is : ",net_salary)
-
-emp1 = Employee(1000,400,100,"ayoub")
-emp1.calculate_net_salary()
